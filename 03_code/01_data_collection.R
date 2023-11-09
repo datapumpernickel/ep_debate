6a246f7a0f41d1d98ebb6974b0e6439c66deede2
Bochtler, Paul <65470117+datapumpernickel@users.noreply.github.com>
2023-11-09 16:03:31 +0100

    some data cleaning and merging translation back to original dataframe

--- 03_code/01_data_collection.R
+++ 03_code/01_data_collection.R
@@ -76,70 +76,70 @@ all_tops <- future_map_dfr(paths, parse_tocs, .progress = T)
 # Data Cleaning and Filtering -------------------------------------------------------------------------------------
 
 ## check which ones might be debates
-
-frequent_tocs_not_debates <- all_tops |>
-  filter(!str_detect(orders,'\\(debate\\)')) |>
-  mutate(orders = tolower(orders) |> str_squish()) |>
-  mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
-  mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
-  count(orders) |>
-  filter(!n<2)|>
-  filter(!str_detect(orders,'one-minute speeches')) |>
-  filter(!str_detect(orders,'human rights'))|>
-  filter(!str_detect(orders,'topical and urgent debate'))|>
-  filter(!str_detect(orders,'zimbabwe'))|>
-  filter(!str_detect(orders,'situation in the middle east')) |>
-  filter(!n<10)
-
-
-unfrequent_tocs_greater_one_not_debates <- all_tops |>
-  filter(!str_detect(orders,'\\(debate\\)')) |>
-  mutate(orders = tolower(orders) |> str_squish()) |>
-  mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
-  mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
-  count(orders) |>
-  filter(!n<2)|>
-  filter(!str_detect(orders,'one-minute speeches')) |>
-  filter(!str_detect(orders,'human rights'))|>
-  filter(!str_detect(orders,'topical and urgent debate'))|>
-  filter(!str_detect(orders,'zimbabwe'))|>
-  filter(!str_detect(orders,'situation in the middle east')) |>
-  filter(n<10)
-
-unfrequent_tocs_greater_one_debates <- all_tops |>
-  filter(!str_detect(orders,'\\(debate\\)')) |>
-  mutate(orders = tolower(orders) |> str_squish()) |>
-  mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
-  mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
-  count(orders) |>
-  filter(!n<2)|>
-  filter(!str_detect(orders,'one-minute speeches')) |>
-  filter(!str_detect(orders,'human rights'))|>
-  filter(!str_detect(orders,'topical and urgent debate'))|>
-  filter(!str_detect(orders,'zimbabwe'))|>
-  filter(!str_detect(orders,'situation in the middle east'))|>
-  filter(n<10) |>
-  filter(!str_detect(orders,'vote|discharge|amendment|negotiation|resumption|welcome|adjournment|action taken|address by|corrigend|composition of|third voting|voting time|written declaration|written statement|statement by the president|approval of the|change to the agenda|delegated acts|^election of the|formal sitting|oral question|request for|signature of|other business|consent procedure|calendar of part-session|membership of'))
-
-unfrequent_tocs_greater_one_not_debates <- anti_join(unfrequent_tocs_greater_one_not_debates,
-                                                     unfrequent_tocs_greater_one_debates, by = 'orders')
-
-only_debates <- all_tops |>
-  mutate(orig_orders = orders,
-         orders = tolower(orders) |> str_squish()) |>
-  anti_join(frequent_tocs_not_debates, by = 'orders') |>
-  anti_join(unfrequent_tocs_greater_one_not_debates, by = 'orders') |>
-  filter(!str_detect(orders,'votes'))|>
-  filter(!str_detect(orders,'motion of censure'))|>
-  filter(!str_detect(orders,'opening of the session'))|>
-  filter(!str_detect(orders,'closing of the session'))|>
-  filter(!str_detect(orders,'adjournment of the session'))|>
-  filter(!str_detect(orders,'question time '))|>
-  filter(!str_detect(orders,'order of business ')) |>
-  mutate(id = str_remove_all(orders_ref,'/doceo/document/')) |>
-  mutate(orders_ref = str_c('https://www.europarl.europa.eu',orders_ref)) |> 
-  distinct(id, orig_orders,path, orders_ref)
-
+# 
+# frequent_tocs_not_debates <- all_tops |>
+#   filter(!str_detect(orders,'\\(debate\\)')) |>
+#   mutate(orders = tolower(orders) |> str_squish()) |>
+#   mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
+#   mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
+#   count(orders) |>
+#   filter(!n<2)|>
+#   filter(!str_detect(orders,'one-minute speeches')) |>
+#   filter(!str_detect(orders,'human rights'))|>
+#   filter(!str_detect(orders,'topical and urgent debate'))|>
+#   filter(!str_detect(orders,'zimbabwe'))|>
+#   filter(!str_detect(orders,'situation in the middle east')) |>
+#   filter(!n<10)
+# 
+# 
+# unfrequent_tocs_greater_one_not_debates <- all_tops |>
+#   filter(!str_detect(orders,'\\(debate\\)')) |>
+#   mutate(orders = tolower(orders) |> str_squish()) |>
+#   mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
+#   mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
+#   count(orders) |>
+#   filter(!n<2)|>
+#   filter(!str_detect(orders,'one-minute speeches')) |>
+#   filter(!str_detect(orders,'human rights'))|>
+#   filter(!str_detect(orders,'topical and urgent debate'))|>
+#   filter(!str_detect(orders,'zimbabwe'))|>
+#   filter(!str_detect(orders,'situation in the middle east')) |>
+#   filter(n<10)
+# 
+# unfrequent_tocs_greater_one_debates <- all_tops |>
+#   filter(!str_detect(orders,'\\(debate\\)')) |>
+#   mutate(orders = tolower(orders) |> str_squish()) |>
+#   mutate(orders = str_remove_all(orders,'^ \\d{1,2}. ')) |>
+#   mutate(orders = str_remove_all(orders,'^\\d{1,2}. ')) |>
+#   count(orders) |>
+#   filter(!n<2)|>
+#   filter(!str_detect(orders,'one-minute speeches')) |>
+#   filter(!str_detect(orders,'human rights'))|>
+#   filter(!str_detect(orders,'topical and urgent debate'))|>
+#   filter(!str_detect(orders,'zimbabwe'))|>
+#   filter(!str_detect(orders,'situation in the middle east'))|>
+#   filter(n<10) |>
+#   filter(!str_detect(orders,'vote|discharge|amendment|negotiation|resumption|welcome|adjournment|action taken|address by|corrigend|composition of|third voting|voting time|written declaration|written statement|statement by the president|approval of the|change to the agenda|delegated acts|^election of the|formal sitting|oral question|request for|signature of|other business|consent procedure|calendar of part-session|membership of'))
+# 
+# unfrequent_tocs_greater_one_not_debates <- anti_join(unfrequent_tocs_greater_one_not_debates,
+#                                                      unfrequent_tocs_greater_one_debates, by = 'orders')
+# 
+# only_debates <- all_tops |>
+#   mutate(orig_orders = orders,
+#          orders = tolower(orders) |> str_squish()) |>
+#   anti_join(frequent_tocs_not_debates, by = 'orders') |>
+#   anti_join(unfrequent_tocs_greater_one_not_debates, by = 'orders') |>
+#   filter(!str_detect(orders,'votes'))|>
+#   filter(!str_detect(orders,'motion of censure'))|>
+#   filter(!str_detect(orders,'opening of the session'))|>
+#   filter(!str_detect(orders,'closing of the session'))|>
+#   filter(!str_detect(orders,'adjournment of the session'))|>
+#   filter(!str_detect(orders,'question time '))|>
+#   filter(!str_detect(orders,'order of business ')) |>
+#   mutate(id = str_remove_all(orders_ref,'/doceo/document/')) |>
+#   mutate(orders_ref = str_c('https://www.europarl.europa.eu',orders_ref)) |> 
+#   distinct(id, orig_orders,path, orders_ref)
+# 
 
 
 # Get Debates -------------------------------------------------------------------------------------
@@ -196,10 +196,7 @@ if(parse_debates_logical) {
 # fs::file_move("04_clean_data/02_clean_data/parsed_debates_raw.rds","04_clean_data/parsed_debates_raw.rds")
 # fs::dir_delete("04_clean_data/02_clean_data")
 
-speeches <- read_rds("04_clean_data/parsed_debates_raw.rds")
-
-
-speeches_clean <- speeches |>
+speeches_clean <- read_rds("04_clean_data/parsed_debates_raw.rds") |>
   left_join(
     debates_links |> mutate(
       path_toc = path,
@@ -243,7 +240,7 @@ parlspeech <- read_csv("01_raw_data/ext_dta/parlee_ep_plenary_speeches.csv") |>
 
 future::plan("multisession", workers = 12)
 
-speeches_clean_add <- speeches_clean |>
+speeches_collapsed <- speeches_clean |>
   filter(!date_clean %in% parlspeech$date_clean) |>
   group_by(path_cre) |>
   mutate(id_speaker = consecutive_id(speaker)) |>
@@ -251,7 +248,7 @@ speeches_clean_add <- speeches_clean |>
   group_by(path_cre, id_speaker) |>
   mutate(p_id = row_number()) |>
   ungroup() |>
-  mutate(text = future_pmap_chr(list(p_id, text, speaker, party, italics),
+  mutate(text = future_pmap_chr(list(p_id, text, speaker, party, italics), ## clean mention of party and speaker from text itself 
                                 function(p_id, text, speaker, party, italics) {
                                   if (p_id == 1) {
                                     if (!is.na(speaker)) {
@@ -274,27 +271,66 @@ speeches_clean_add <- speeches_clean |>
                                   return(text)
                                   
                                 },
-                                .progress = T))
-
-speeches_collapsed <- speeches_clean_add |> 
-  group_by(date_clean, speaker, path_cre, path_toc, orig_orders, url_cre, id_speaker) |> 
-  summarise(text = paste0(text |> str_squish(), collapse = "\n"),
-            italics =paste0(italics |> str_squish(), collapse = "\n"), 
-            party =paste0(party |> str_squish(), collapse = "\n"))
-
-future::plan("multisession", workers = 4)
-
-speeches_clean_missing <- speeches_collapsed |> 
-  select(date_clean, id_speaker, speaker, party, italics, text, url_cre, everything() ) |> 
-  ungroup() |> 
-  mutate(party = future_map_chr(party,~str_split_1(.x,pattern = "\\n")|> unique() |> paste0(collapse = ";"),.progress = T) ,
-         italics = future_map_chr(italics, ~str_split_1(.x,pattern = "\\n")|> unique() |> paste0(collapse = ";"),.progress = T)) |> 
-  mutate(text_id = cur_group_id(),.by = c(url_cre, id_speaker))|> 
-  arrange(date_clean, url_cre) |> 
-  mutate(session_id = cur_group_id(),.by = c(url_cre)) |> 
-  filter(!is.na(text)) |> 
+                                .progress = T)) |>
+  group_by(date_clean,
+           speaker,
+           path_cre,
+           path_toc,
+           orig_orders,
+           url_cre,
+           id_speaker) |>
+  summarise( ## collapse texts
+    text = paste0(text |> str_squish(), collapse = "\n"),
+    italics = paste0(italics |> str_squish(), collapse = "\n"),
+    party = paste0(party |> str_squish(), collapse = "\n")
+  ) |>
+  select(date_clean,
+         id_speaker,
+         speaker,
+         party,
+         italics,
+         text,
+         url_cre,
+         everything()) |>
+  ungroup() |>
+  mutate( ## keep only unique party and italics mentions for each collapsed speaker/
+    party = future_map_chr(
+      party,
+      ~ str_split_1(.x, pattern = "\\n") |> unique() |> paste0(collapse = ";"),
+      .progress = T
+    ) ,
+    italics = future_map_chr(
+      italics,
+      ~ str_split_1(.x, pattern = "\\n") |> unique() |> paste0(collapse = ";"),
+      .progress = T
+    )
+  ) |>
+  mutate(text_id = cur_group_id(), .by = c(url_cre, id_speaker)) |>
+  arrange(date_clean, url_cre) |>
+  mutate(session_id = cur_group_id(), .by = c(url_cre)) |>
+  filter(!is.na(text)) |>
   select(text_id, session_id, everything())
 
 ## write speeches that are missing in Parlee dataset
 
-write_csv(speeches_clean_missing, "04_clean_data/missing_speeches_parlee.csv" )
+write_csv(speeches_collapsed, "04_clean_data/missing_speeches_parlee.csv")
+
+
+### tokenization happens with spacy in python (see sentence_segmentation.py)
+
+speeches_add_sents <- read_csv("04_clean_data/missing_speeches_parlee_sents.csv") |> 
+  select(-...1) |> 
+  janitor::clean_names()
+
+speeches_add_clean <- speeches_collapsed |> 
+  select(-text) |> 
+  left_join(speeches_add_sents, by = c("text_id","session_id","id_speaker"),multiple = "all")
+
+future::plan("multisession", workers = 12)
+
+translations <- read_lines("04_clean_data/translations/translated_sentences.jsonl") |> 
+  future_map_dfr(jsonlite::fromJSON)
+
+translations_with_original <- translations |> 
+  janitor::clean_names() |> 
+  left_join(speeches_add_clean |> select(sentence, text_id, sentence_id))
